68bfaf2024ee50c26d94463835c39e52c055f51c
Vladimir Magamedov <vladimir@magamedov.com>
2017-11-30 14:53:09 +0200

    Added documentation config

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = 'grpclib'
+copyright = '2017, Vladimir Magamedov'
+author = 'Vladimir Magamedov'
+
+version = 'dev'
+release = 'dev'
+
+templates_path = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = []
---
+++ docs/index.rst
@@ -0,0 +1,9 @@
+.. include:: ../README.rst
+
+User's Guide
+~~~~~~~~~~~~
+
+.. toctree::
+  :maxdepth: 2
+
+  changelog/index
