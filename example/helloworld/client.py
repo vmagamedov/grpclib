6db7da3bf0b5511a43592cf4f6fc83dac32effde
claws <clawsicus@gmail.com>
2018-02-27 12:27:39 +0000

    Update example to show supported RPC methods #17

--- example/helloworld/client.py
+++ example/helloworld/client.py
@@ -1,7 +1,6 @@
 import asyncio
 
 from grpclib.client import Channel
-
 from .helloworld_pb2 import HelloRequest
 from .helloworld_grpc import GreeterStub
 
@@ -10,7 +9,62 @@ async def main():
     channel = Channel(loop=asyncio.get_event_loop())
     stub = GreeterStub(channel)
 
-    print(await stub.SayHello(HelloRequest(name='World')))
+    # ------------------------------------------------------------------------
+    # UNARY_UNARY RPC
+
+    print('Demonstrating UNARY_UNARY')
+
+    # Demonstrate simple case where requests are known before interaction
+    print(await stub.UnaryUnaryGreeting(HelloRequest(name='you')))
+
+    # This block performs the same UNARY_UNARY interaction as above
+    # while showing more advanced stream control features.
+    async with stub.UnaryUnaryGreeting.open() as stream:
+        await stream.send_message(HelloRequest(name='yall'))
+        reply = await stream.recv_message()
+        print(reply)
+
+    # ------------------------------------------------------------------------
+    # UNARY_STREAM RPC
+
+    print('Demonstrating UNARY_STREAM')
+
+    # Demonstrate simple case where requests are known before interaction
+    print(await stub.UnaryStreamGreeting(HelloRequest(name='you')))
+
+    # This block performs the same UNARY_STREAM interaction as above
+    # while showing more advanced stream control features.
+    async with stub.UnaryStreamGreeting.open() as stream:
+        await stream.send_message(HelloRequest(name='yall'), end=True)
+        replies = [reply async for reply in stream]
+        print(replies)
+
+    # ------------------------------------------------------------------------
+    # STREAM_UNARY RPC
+
+    print('Demonstrating STREAM_UNARY')
+
+    # Demonstrate simple case where requests are known before interaction
+    msgs = [HelloRequest(name='Rick'), HelloRequest(name='Morty')]
+    print(await stub.StreamUnaryGreeting(msgs))
+
+    # This block performs the same STREAM_UNARY interaction as above
+    # while showing more advanced stream control features.
+    async with stub.StreamUnaryGreeting.open() as stream:
+        for msg in msgs:
+            await stream.send_message(msg)
+        await stream.end()
+        reply = await stream.recv_message()
+        print(reply)
+
+    # ------------------------------------------------------------------------
+    # STREAM_STREAM RPC
+
+    print('Demonstrating STREAM_STREAM')
+
+    # Demonstrate simple case where requests are known before interaction
+    msgs = [HelloRequest(name='Rick'), HelloRequest(name='Morty')]
+    print(await stub.StreamStreamGreeting(msgs))
 
 
 if __name__ == '__main__':
--- example/helloworld/server.py
+++ example/helloworld/server.py
@@ -1,16 +1,42 @@
 import asyncio
 
 from grpclib.server import Server
-
 from .helloworld_pb2 import HelloReply
 from .helloworld_grpc import GreeterBase
 
 
 class Greeter(GreeterBase):
 
-    async def SayHello(self, stream):
+    # UNARY_UNARY - simple RPC
+    async def UnaryUnaryGreeting(self, stream):
         request = await stream.recv_message()
         message = 'Hello, {}!'.format(request.name)
+        await stream.send_message(HelloReply(message=message))
+
+    # UNARY_STREAM - response streaming RPC
+    async def UnaryStreamGreeting(self, stream):
+        request = await stream.recv_message()
+        await stream.send_message(
+            HelloReply(message='Hello, {}!'.format(request.name)))
+        await stream.send_message(
+            HelloReply(message='Goodbye, {}!'.format(request.name)))
+
+    # STREAM_UNARY - request streaming RPC
+    async def StreamUnaryGreeting(self, stream):
+        names = []
+        async for request in stream:
+            names.append(request.name)
+        message = 'Hello, {}!'.format(' and '.join(names))
+        await stream.send_message(HelloReply(message=message))
+
+    # STREAM_STREAM - bidirectional streaming RPC
+    async def StreamStreamGreeting(self, stream):
+        async for request in stream:
+            message = 'Hello, {}!'.format(request.name)
+            await stream.send_message(HelloReply(message=message))
+        # Send another message to demonstrate responses are not
+        # coupled to requests.
+        message = 'Goodbye, all!'
         await stream.send_message(HelloReply(message=message))
 
 
