5c1018b2b9446c8c3f105bcd23d0a5668a31c3c2
Vladimir Magamedov <vladimir@magamedov.com>
2019-07-10 17:11:36 +0300

    Graduated from Beta

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         ],
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
