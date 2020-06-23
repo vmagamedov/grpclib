9a0bd99ec078966dc33a743c734adf1b769b21c8
Vladimir Magamedov <vladimir@magamedov.com>
2020-06-23 23:35:03 +0300

    Fixed test_default_ssl_context and test_concurrent_connect

--- tests/test_client_channel.py
+++ tests/test_client_channel.py
@@ -1,3 +1,4 @@
+import sys
 import asyncio
 from unittest.mock import patch, ANY
 
@@ -11,31 +12,27 @@ from dummy_grpc import DummyServiceStub
 from test_functional import DummyService
 
 
-async def _create_connection(protocol):
-    await asyncio.sleep(0.01)
-    return None, protocol
-
-
-def _create_connection_gen(protocol):
-    while True:
-        yield _create_connection(protocol)
-
-
 @pytest.mark.asyncio
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="Python < 3.8")
 async def test_concurrent_connect(loop):
     count = 5
     reqs = [DummyRequest(value='ping') for _ in range(count)]
     reps = [DummyReply(value='pong') for _ in range(count)]
 
     channel = Channel()
+
+    async def create_connection(*args, **kwargs):
+        await asyncio.sleep(0.01)
+        return None, _channel._protocol
+
     stub = DummyServiceStub(channel)
     async with ChannelFor([DummyService()]) as _channel:
         with patch.object(loop, 'create_connection') as po:
-            po.side_effect = _create_connection_gen(_channel._protocol)
+            po.side_effect = create_connection
             tasks = [loop.create_task(stub.UnaryUnary(req)) for req in reqs]
             replies = await asyncio.gather(*tasks)
     assert replies == reps
-    po.assert_called_once_with(ANY, '127.0.0.1', 50051, ssl=None)
+    po.assert_awaited_once_with(ANY, '127.0.0.1', 50051, ssl=None)
 
 
 def test_default_ssl_context():
@@ -47,5 +44,4 @@ def test_default_ssl_context():
     system_certs = system_channel._ssl.get_ca_certs(binary_form=True)
 
     assert certifi_certs
-    assert system_certs
     assert certifi_certs != system_certs
