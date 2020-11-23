vmagamedov/grpclib

commit e6ce8b67512f3431c677e8fc9ccd3fd4878e7f99

Fixed asyncio.wait() usage in the Server.wait_closed() coroutine function. Fixes #124.

grpclib/server.py
@@ -729,4 +729,6 @@ class Server(_GC, asyncio.AbstractServer):
             raise RuntimeError('Server is not started')
         await self._server.wait_closed()
         if self._handlers:
-            await asyncio.wait({h.wait_closed() for h in self._handlers})
+            await asyncio.wait({
+                asyncio.create_task(h.wait_closed()) for h in self._handlers
+            })

commit b15413c954d76cc1874b1d52e25d97871f6346b9

Fixed Python 3.6 support broken in e6ce8b67512f3431c677e8fc9ccd3fd4878e7f99

grpclib/server.py
@@ -730,5 +730,5 @@ class Server(_GC, asyncio.AbstractServer):
         await self._server.wait_closed()
         if self._handlers:
             await asyncio.wait({
-                asyncio.create_task(h.wait_closed()) for h in self._handlers
+                self._loop.create_task(h.wait_closed()) for h in self._handlers
             })