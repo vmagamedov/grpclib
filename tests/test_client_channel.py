9b0f8fe69e59bef96893119fdbbd874c13413415
Vladimir Magamedov <vladimir@magamedov.com>
2023-10-02 14:32:26 +0300

    Tested ssl_target_name_override config option

--- tests/test_client_channel.py
+++ tests/test_client_channel.py
@@ -4,21 +4,25 @@ from unittest.mock import patch, ANY
 
 import pytest
 import certifi
+from h2.connection import H2Connection
 
-from grpclib.client import Channel
+from grpclib.client import Channel, Handler
+from grpclib.config import Configuration
+from grpclib.protocol import H2Protocol
 from grpclib.testing import ChannelFor
 
 from dummy_pb2 import DummyRequest, DummyReply
 from dummy_grpc import DummyServiceStub
 from test_functional import DummyService
+from stubs import TransportStub
 
 
 @pytest.mark.asyncio
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="Python < 3.8")
 async def test_concurrent_connect(loop):
     count = 5
-    reqs = [DummyRequest(value='ping') for _ in range(count)]
-    reps = [DummyReply(value='pong') for _ in range(count)]
+    reqs = [DummyRequest(value="ping") for _ in range(count)]
+    reps = [DummyReply(value="pong") for _ in range(count)]
 
     channel = Channel()
 
@@ -28,12 +32,18 @@ async def test_concurrent_connect(loop):
 
     stub = DummyServiceStub(channel)
     async with ChannelFor([DummyService()]) as _channel:
-        with patch.object(loop, 'create_connection') as po:
+        with patch.object(loop, "create_connection") as po:
             po.side_effect = create_connection
             tasks = [loop.create_task(stub.UnaryUnary(req)) for req in reqs]
             replies = await asyncio.gather(*tasks)
     assert replies == reps
-    po.assert_awaited_once_with(ANY, '127.0.0.1', 50051, ssl=None)
+    po.assert_awaited_once_with(
+        ANY,
+        "127.0.0.1",
+        50051,
+        ssl=None,
+        server_hostname=None,
+    )
 
 
 def test_default_ssl_context():
@@ -46,3 +56,24 @@ def test_default_ssl_context():
         with patch.dict("sys.modules", {"certifi": None}):
             system_channel = Channel(ssl=True)
             assert system_channel._ssl
+
+
+@pytest.mark.asyncio
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="Python < 3.8")
+async def test_ssl_target_name_override(loop):
+    config = Configuration(ssl_target_name_override="example.com")
+
+    async def create_connection(*args, **kwargs):
+        h2_conn = H2Connection()
+        transport = TransportStub(h2_conn)
+        protocol = H2Protocol(Handler(), config.__for_test__(), h2_conn.config)
+        protocol.connection_made(transport)
+        return None, protocol
+
+    with patch.object(loop, "create_connection") as po:
+        po.side_effect = create_connection
+        async with Channel(ssl=True, config=config) as channel:
+            await channel.__connect__()
+            po.assert_awaited_once_with(
+                ANY, ANY, ANY, ssl=channel._ssl, server_hostname="example.com"
+            )
